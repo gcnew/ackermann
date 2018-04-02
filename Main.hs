69931ff8f5dd9757e033f2fc09542d4c416b66c1
gcnew <gcnew@abv.bg>
2018-04-02 04:16:42 +0300

    Implementation with rr

--- Main.hs
+++ Main.hs
@@ -1,37 +1,13 @@
 {-# LANGUAGE Rank2Types #-}
 
-import Prelude hiding (const, succ, pred, fst, snd)
+rr :: a -> (Int -> (a -> a)) -> (Int -> a)
+rr i f 0 = i
+rr i f n = f (n-1) (rr i f (n-1))
 
-type LambdaInt = forall a. (a -> a) -> a -> a
+next :: Int -> Int
+next = (+ 1)
 
-zero f x = x
-succ n f x = f (n f x)
-pred n = snd (n g s)
-    where s   = pair false zero
-          g p = pair true (p (\f s -> f succ id s))
+ack :: Int -> Int -> Int
+ack m = rr next (\_ f n -> rr (f 1) (const f) n) m
 
-          -- s = (False, zero)
-          -- g (False, f) = (True, f)
-          -- g (True, f)  = (True, succ f)
-
--- from Wikipedia (slower):
--- pred n f x = n (\g h -> h (g f)) (\_ -> x) (\u -> u)
-
-pair l r f = f l r
-fst p = p (\l r -> l)
-snd p = p (\l r -> r)
-
-true  t f = t
-false t f = f
-
-const x _ = x
-
-isZero n = n (const false) true
-
-ackermann :: LambdaInt -> LambdaInt -> LambdaInt
-ackermann x y = isZero x (succ y) $
-                isZero y (ackermann (pred x) (succ zero)) $
-                ackermann (pred x) (ackermann x (pred y))
-
-
-main = print $ ackermann (succ . succ .  succ $ zero) (succ . succ . succ . succ $ zero) (+1) 0
+main = print $ ack 3 4
